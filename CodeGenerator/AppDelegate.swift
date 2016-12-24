3416edf8fa3913559e044771769948961e515cc9
WANG Jie <jacques09125715@gmail.com>
2016-12-25 00:21:32 +0100

    Initial Commit

---
+++ CodeGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodeGenerator
+//
+//  Created by WANG Jie on 25/12/2016.
+//  Copyright © 2016 wangjie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CodeGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeGenerator
+//
+//  Created by WANG Jie on 25/12/2016.
+//  Copyright © 2016 wangjie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
