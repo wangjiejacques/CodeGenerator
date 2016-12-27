24f26fbef6b987879f93ac6ac8f63df07d89f76a
WANG Jie <jacques09125715@gmail.com>
2016-12-27 17:23:55 +0100

    implement GenerateEquatableCommand

--- Generator.../GenerateEquatableCommand.swift
+++ Generator.../GenerateEquatableCommand.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import XcodeKit
+
+class GenerateEquatableCommand: NSObject, XCSourceEditorCommand {
+
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        let interfaceSignature = InterfaceSignature(interfaceSource: invocation.buffer.completeBuffer, lines: invocation.buffer.lines.map { $0 as! String })
+        let equatableGenerator = EquatableGenerator(interfaceSignature: interfaceSignature, indentation: Array(repeating: " ", count: invocation.buffer.indentationWidth).joined())
+        invocation.buffer.lines.addObjects(from: equatableGenerator.lines)
+        completionHandler(nil)
+    }
+}
