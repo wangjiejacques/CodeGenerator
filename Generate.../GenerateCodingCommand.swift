b9996880a217eebdb8ac8d83d51df112b11fc54e
WANG Jie <jacques09125715@gmail.com>
2017-05-25 12:39:42 +0200

    add NSCoding command

---
+++ Generate.../GenerateCodingCommand.swift
@@ -0,0 +1,21 @@
+//
+//  GenerateCodingCommand.swift
+//  CodeGenerator
+//
+//  Created by WANG Jie on 25/05/2017.
+//  Copyright © 2017 wangjie. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class GenerateCodingCommand: NSObject, GenerateAsExtensionCommand {
+
+    var generatorType: Generator.Type {
+        return CodingGenerator.self
+    }
+
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
+        swiftPerform(with: invocation, completionHandler: completionHandler)
+    }
+}
